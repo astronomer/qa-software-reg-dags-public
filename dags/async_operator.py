e3b42c024fc832eddeb15c9bf4b031828c8c1ff1
Priyanka13Astro <priyanka+13@astronomer.io>
2023-10-30 16:38:51 +0530

    added task to dag

--- dags/async_operator.py
+++ dags/async_operator.py
@@ -5,7 +5,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.sensors.time_sensor import TimeSensorAsync
 from airflow.utils.dates import days_ago
 from airflow.utils.timezone import utcnow
-
+from airflow.sensors.bash import BashSensor
 
 default_args = {"start_date": days_ago(2)}
 
@@ -15,5 +15,6 @@ with DAG("async_operator", schedule_interval="@daily", default_args=default_args
         task_id="sensor",
         target_time=(utcnow() + datetime.timedelta(seconds=10)).time(),
     )
+    task1 = BashSensor(task_id="sleep_10", bash_command="sleep 10")
     complete = DummyOperator(task_id="complete")
-    sensor >> complete
+    sensor >> task1 >> complete
